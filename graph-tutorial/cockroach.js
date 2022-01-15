4facd5f2c7d6ed448ea22123d47bc1ef956526f9
Mayank Govilla <mgovilla@gmail.com>
2022-01-15 17:08:24 -0500

    add ssl for connection

--- graph-tutorial/cockroach.js
+++ graph-tutorial/cockroach.js
@@ -1,20 +1,23 @@
 require('dotenv').config()
+const fs = require('fs')
 
 const { Pool, Client } = require('pg')
-const connectionString = process.env.CONNECTION_STRING
+const config = {
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(process.env.HOME + '/.postgresql/root.crt').toString()
+    }
+}
 
-const pool = new Pool({
-  connectionString,
-})
+const pool = new Pool(config)
 
 pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   pool.end()
 })
 
-const client = new Client({
-  connectionString,
-})
+const client = new Client(config)
 
 client.connect()
 client.query('SELECT NOW()', (err, res) => {
