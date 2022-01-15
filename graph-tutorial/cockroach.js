5cf1fecbfc6008aaa25c9787d68ab87e2cccf9a8
Mayank Govilla <mgovilla@gmail.com>
2022-01-15 18:30:39 -0500

    add create event function

--- graph-tutorial/cockroach.js
+++ graph-tutorial/cockroach.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const fs = require('fs')
 
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
 const config = {
     connectionString: process.env.CONNECTION_STRING,
     ssl: {
@@ -13,14 +13,24 @@ const config = {
 const pool = new Pool(config)
 
 pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
+    console.log(err, res)
+    pool.end()
 })
 
-const client = new Client(config)
+function createEvent(event) {
+    // callback - checkout a client
+    pool.connect((err, client, done) => {
+        if (err) throw err
+        client.query("INSERT INTO EVENT VALUES(gen_random_uuid(), $1, $2, $3)", [event.name, event.start, event.end], (err, res) => {
+            done(res)
+            console.log(res)
+            if (err) {
+                console.log(err.stack)
+            } else {
+                console.log(res.rows[0])
+            }
+        })
+    })
+}
 
-client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+createEvent({name: 'test', start: '2022-01-15 01:00:00', end: '2022-01-1 04:05:06'})
