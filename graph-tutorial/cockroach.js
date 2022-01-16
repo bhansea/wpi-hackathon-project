b1340748b6a8ac804a83c8b55e136eadf27276c8
Mayank Govilla <mgovilla@gmail.com>
2022-01-16 00:04:05 -0500

    finish db queries

--- graph-tutorial/cockroach.js
+++ graph-tutorial/cockroach.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const fs = require('fs')
-
 const { Pool } = require('pg')
+
 const config = {
     connectionString: process.env.CONNECTION_STRING,
     ssl: {
@@ -12,25 +12,31 @@ const config = {
 
 const pool = new Pool(config)
 
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-})
-
-function createEvent(event) {
-    // callback - checkout a client
-    pool.connect((err, client, done) => {
-        if (err) throw err
-        client.query("INSERT INTO EVENT VALUES(gen_random_uuid(), $1, $2, $3)", [event.name, event.start, event.end], (err, res) => {
-            done(res)
-            console.log(res)
-            if (err) {
-                console.log(err.stack)
-            } else {
-                console.log(res.rows[0])
-            }
-        })
-    })
+async function createEvent(event) {
+    return (await pool.query("INSERT INTO EVENT VALUES(gen_random_uuid(), $1, $2, $3) RETURNING id", [event.name, event.start, event.end])).rows
 }
 
-createEvent({name: 'test', start: '2022-01-15 01:00:00', end: '2022-01-1 04:05:06'})
+async function addUserToIncrement(event, increment) {
+        qString = ` insert into increment values($1, $2, ARRAY[$3]) 
+                    ON CONFLICT (event_id, idx) DO UPDATE SET users = array_append(increment.users, $3) 
+                    WHERE increment.event_id = $1 AND increment.idx=$2;`
+
+        await pool.query(qString, [event.id, increment.index, increment.user])
+}
+
+async function getEvent(id) {
+    qString = `SELECT * FROM INCREMENT WHERE event_id = $1`
+
+    return (await pool.query(qString, [id])).rows
+}
+
+// getEvent('30ec7002-8ffe-4cf5-ab09-4ffbe806a632').then((result) => console.log(result)).then(()=>{})
+// dbEvent = {name: 'new test event', start: '2022-01-25 03:00:00', end: '2022-01-25 05:00:00'}
+// createEvent(dbEvent).then((result) => console.log(result)).then(()=>{})
+// addUserToIncrement({id: '30ec7002-8ffe-4cf5-ab09-4ffbe806a632'}, {index: 3, user: 'darren'})
+
+module.exports = {
+    createEvent,
+    addUserToIncrement,
+    getEvent
+}
